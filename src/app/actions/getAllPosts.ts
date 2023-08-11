8006d1ddfd4d2128755d033b4af5ba01aab7cbe0
Denisssska <yarmoshkoden18m@gmail.com>
2023-08-11 14:35:21 +0300

    ci: commit

--- src/app/actions/getAllPosts.ts
+++ src/app/actions/getAllPosts.ts
@@ -1,7 +1,9 @@
 import { IPost } from '#/types';
 
 export async function getAllPosts(): Promise<Array<IPost>> {
-  const res = await fetch('http://localhost:3000/api/posts');
+  const res = await fetch('http://localhost:3000/api/posts', {
+    cache: 'no-store',
+  });
 
   if (!res.ok) {
     throw new Error('Failed to fetch data');
