85d0b8e743f7cc5d98da2f9fa8615d30ddfd5637
Denisssska <yarmoshkoden18m@gmail.com>
2023-08-11 14:03:41 +0300

    ci: first complete app

--- src/app/actions/getOnePost.ts
+++ src/app/actions/getOnePost.ts
@@ -1,7 +1,9 @@
 import { IPost } from '#/types';
 
 export async function getOnePost(id: string): Promise<IPost> {
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`);
+  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+    cache: 'no-store',
+  });
 
   if (!res.ok) {
     throw new Error('Failed to fetch data');
