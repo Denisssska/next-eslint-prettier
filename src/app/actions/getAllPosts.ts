10288880c4e03b50fefdb6e1820fcb761dc6722e
Denisssska <yarmoshkoden18m@gmail.com>
2023-08-17 19:02:33 +0300

    ci: refactor

--- src/app/actions/getAllPosts.ts
+++ src/app/actions/getAllPosts.ts
@@ -2,7 +2,7 @@ import { IPost } from '#/types';
 
 export async function getAllPosts(): Promise<Array<IPost>> {
   const res = await fetch('http://localhost:3000/api/posts', {
-    cache: 'no-store',
+    cache: 'force-cache',
   });
 
   if (!res.ok) {
