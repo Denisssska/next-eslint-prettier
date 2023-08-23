31a6c64c819553b728af045646ec0e9164ac7859
Denisssska <yarmoshkoden18m@gmail.com>
2023-08-23 12:53:06 +0300

    ci: add meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import './globals.scss';
 import { Inter } from 'next/font/google';
 
 import { ClientOnlyProps } from '#/types';
@@ -9,13 +10,28 @@ import { ThemeProvider } from '@/context/ThemeContext';
 
 import type { Metadata } from 'next';
 
-import './globals.scss';
-
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Геодезические работы',
-  description: 'Геодезические работы в Беларуси',
+  generator: 'Next.js',
+  applicationName: 'Next-Blog',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next.js', 'React', 'TypeScript'],
+  authors: [
+    { name: 'Den' },
+    {
+      name: 'Denis',
+      url: 'https://drive.google.com/file/d/1najLFBrKwE3W4EyJeJGFZRra7oZ68mZ8/view?usp=sharing',
+    },
+  ],
+  colorScheme: 'dark',
+  creator: 'Denis Yarmoshko',
+  publisher: 'Yarmoshko Denis',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
 };
 
 export default function RootLayout({ children }: ClientOnlyProps) {
